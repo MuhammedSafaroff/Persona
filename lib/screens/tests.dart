638a2187cf1065bf9810126158dad87200c779b9
LambsBread <leokingerozum@gmail.com>
2021-11-05 08:26:33 +0100

    Fixed Incorrect Format Build Bug

--- lib/screens/tests.dart
+++ lib/screens/tests.dart
@@ -3,27 +3,104 @@ import 'package:flutter/material.dart';
 import 'package:survey_kit/survey_kit.dart';
 
 class Tests extends StatelessWidget {
-  Tests({Key? key}) : super(key: key);
+  const Tests({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: FutureBuilder<Task>(
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        child: Align(
+          alignment: Alignment.center,
+          child: FutureBuilder<Task>(
             future: getSampleTask(),
             builder: (context, snapshot) {
+              //async(snapshot)
               if (snapshot.connectionState == ConnectionState.done &&
                   snapshot.hasData &&
                   snapshot.data != null) {
                 final task = snapshot.data!;
                 return SurveyKit(
+                  //Survey Widget
                   onResult: (SurveyResult result) {
-                    print(result.finishReason);
+                    //print(result.finishReason);   //TODO SAVE DATA
                   },
                   task: task,
+                  themeData: Theme.of(context).copyWith(
+                    colorScheme: ColorScheme.fromSwatch(
+                      primarySwatch: Colors.cyan,
+                    ).copyWith(
+                      onPrimary: Colors.white,
+                    ),
+                    primaryColor: Colors.cyan,
+                    backgroundColor: Colors.white,
+                    appBarTheme: const AppBarTheme(
+                      color: Colors.white,
+                      iconTheme: IconThemeData(
+                        color: Colors.cyan,
+                      ),
+                    ),
+                    iconTheme: const IconThemeData(
+                      color: Colors.cyan,
+                    ),
+                    outlinedButtonTheme: OutlinedButtonThemeData(
+                      style: ButtonStyle(
+                        minimumSize: MaterialStateProperty.all(
+                          const Size(150.0, 60.0),
+                        ),
+                        side: MaterialStateProperty.resolveWith(
+                          (Set<MaterialState> state) {
+                            if (state.contains(MaterialState.disabled)) {
+                              return const BorderSide(
+                                color: Colors.grey,
+                              );
+                            }
+                            return const BorderSide(
+                              color: Colors.cyan,
+                            );
+                          },
+                        ),
+                        shape: MaterialStateProperty.all(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                        ),
+                        textStyle: MaterialStateProperty.resolveWith(
+                          (Set<MaterialState> state) {
+                            if (state.contains(MaterialState.disabled)) {
+                              return Theme.of(context)
+                                  .textTheme
+                                  .button
+                                  ?.copyWith(
+                                    color: Colors.grey,
+                                  );
+                            }
+                            return Theme.of(context).textTheme.button?.copyWith(
+                                  color: Colors.cyan,
+                                );
+                          },
+                        ),
+                      ),
+                    ),
+                    textButtonTheme: TextButtonThemeData(
+                      style: ButtonStyle(
+                        textStyle: MaterialStateProperty.all(
+                          Theme.of(context).textTheme.button?.copyWith(
+                                color: Colors.cyan,
+                              ),
+                        ),
+                      ),
+                    ),
+                  ),
                 );
               }
-              return CircularProgressIndicator.adaptive();
-            }));
+              //Navigator.of(context).maybePop();
+              return const CircularProgressIndicator.adaptive();
+            },
+          ),
+        ),
+      ),
+    );
   }
 
   Future<Task> getSampleTask() {
